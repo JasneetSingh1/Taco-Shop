0428a37bc2447e38716397cd96588f192c04e975
Jasneet <jasneetpalsingh619@gmail.com>
2024-01-07 13:31:08 -0700

    replaced the home controller with a view controller

--- src/test/java/com/example/tacocloud/HomeControllerTest.java
+++ src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -6,13 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import com.example.tacocloud.controllers.HomeController;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 public class HomeControllerTest {
     @Autowired  private MockMvc mockMvc;
     @Test  public void testHomePage() throws Exception {
